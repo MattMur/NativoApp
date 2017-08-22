5b3f41bcfc537bed9a60897cfce7075ec75de89b
mattMur <mmurray@nativo.net>
2017-08-21 21:48:16 -0700

    build changes

--- platforms/ios/build/emulator/NativoApp.app/app/views/campaigns/campaigns.ts
+++ platforms/ios/build/emulator/NativoApp.app/app/views/campaigns/campaigns.ts
@@ -7,25 +7,27 @@ logic, and to set up your page’s data binding.
 import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { CampaignsModel } from '../../view-models/campaigns-view-model';
+import { UserViewModel } from '../../view-models/user-view-model';
+import * as frameModule from 'ui/frame';
 
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 export function navigatingTo(args: EventData) {
+    
     /*
     This gets a reference this page’s <Page> UI component. You can
     view the API reference of the Page to see what’s available at
     https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
     */
     let page = <Page>args.object;
-    
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and TypeScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
 
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = new CampaignsModel();
+
+    // Check if user is logged in
+    let user = new UserViewModel();
+    if (!user.isLoggedIn) {
+        page.showModal('../login/login-page', null, () => {
+            page.bindingContext = new CampaignsModel();
+        })
+    } else {
+        page.bindingContext = new CampaignsModel();
+    }
 }
